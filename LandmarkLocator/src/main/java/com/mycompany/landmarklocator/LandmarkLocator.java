93f3330f7e3dedd2babd9d41506d5db59e222cb6
ryanwentz23 <ryanwentz23@gmail.com>
2024-04-09 16:21:25 -0400

    initial commit

---
+++ LandmarkLocator/src/main/java/com/mycompany/landmarklocator/LandmarkLocator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.landmarklocator;
+
+/**
+ *
+ * @author ryanw
+ */
+public class LandmarkLocator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
