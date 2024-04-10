9c6da1ae47a5388fe45cddc892afe4b19b2addc0
EarthCow <coppergriffing@gmail.com>
2024-04-10 13:54:44 -0400

    Adds an API demo

---
+++ src/main/java/com/mycompany/landmarklocator/Demo.java
@@ -0,0 +1,63 @@
+
+package com.mycompany.landmarklocator;
+
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.LocationInfo;
+import com.google.protobuf.ByteString;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CMPSC 221 Program LandmarkLocator
+ * Demo.java
+ * Purpose: Demo for the Google Vision API
+ * 
+ * @version 1.0 Apr 10, 2024
+ */
+public class Demo {
+  public static void detectLandmarks() throws IOException {
+    String filePath = "leaning-tower-of-pisa-getty.jpg";
+    detectLandmarks(filePath);
+  }
+
+  // Detects landmarks in the specified local image.
+  public static void detectLandmarks(String filePath) throws IOException {
+    List<AnnotateImageRequest> requests = new ArrayList<>();
+    ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+    Image img = Image.newBuilder().setContent(imgBytes).build();
+    Feature feat = Feature.newBuilder().setType(Feature.Type.LANDMARK_DETECTION).build();
+    AnnotateImageRequest request =
+        AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+    requests.add(request);
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+      BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+      List<AnnotateImageResponse> responses = response.getResponsesList();
+
+      for (AnnotateImageResponse res : responses) {
+        if (res.hasError()) {
+          System.out.format("Error: %s%n", res.getError().getMessage());
+          return;
+        }
+
+        // For full list of available annotations, see http://g.co/cloud/vision/docs
+        for (EntityAnnotation annotation : res.getLandmarkAnnotationsList()) {
+          LocationInfo info = annotation.getLocationsList().listIterator().next();
+          System.out.format("Landmark: %s%n %s%n", annotation.getDescription(), info.getLatLng());
+        }
+      }
+    }
+  }
+}
--- src/main/java/com/mycompany/landmarklocator/LandmarkLocator.java
+++ src/main/java/com/mycompany/landmarklocator/LandmarkLocator.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.landmarklocator;
 
+import java.io.IOException;
+
 /**
- *
- * @author ryanw
+ * CMPSC 221 Program LandmarkLocator
+ * LandmarkLocator.java
+ * Purpose: Main class for LandmarkLocator
+ * 
+ * @version 1.0 Apr 10, 2024
+ * @author Ryan W & Griffin G
  */
 public class LandmarkLocator {
 
+    /**
+     * Initializes the program
+     * @param args Strings passed to the artifact when ran
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Attempting!");
+        try {
+            Demo.detectLandmarks();
+        } catch (IOException ex) {
+            System.out.println("Failed :(");
+            ex.printStackTrace();
+        }
     }
 }
